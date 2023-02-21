f06f4b980b6981fe3979f0f6b0e3e67b31054378
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-02-21 20:35:04 +0530

    Completed app

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 var socket = io();
 
 var userList = document.getElementById('active_users_list');
-var roomList = document.getElementById('active_room_list');
+var roomList = document.getElementById('active_rooms_list');
 var message = document.getElementById('messageInput');
 var sendMessageBtn = document.getElementById('send_message_btn');
 var roomInput = document.getElementById('roomInput');
@@ -21,9 +21,23 @@ sendMessageBtn.addEventListener('click', function () {
     message.value = '';
 });
 
+message.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        sendMessageBtn.click();
+    }
+});
+
+createRoomBtn.addEventListener('click', function () {
+    let roomName = roomInput.value.trim();
+    if (roomName !== '') {
+        socket.emit('createRoom', roomName);
+        roomInput.value = '';
+    }
+});
+
 socket.on('updateChat', function (username, data) {
     if (username === 'INFO') {
-        chatDisplay.innerHTML = `<div class = 'announcement'><span>${data}</span></div>`;
+        chatDisplay.innerHTML += `<div class = 'announcement'><span>${data}</span></div>`;
     } else {
         chatDisplay.innerHTML += `<div class = 'message_holder ${
             username === myUsername ? 'me' : ''
@@ -39,3 +53,45 @@ socket.on('updateChat', function (username, data) {
     }
     chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
+
+socket.on('updateUsers', (names) => {
+    userList.innerHTML = '';
+    console.log(names);
+    for (let user in names) {
+        if (user === null) {
+            continue;
+        }
+        userList.innerHTML += `<div class = 'user_card'>
+            <div class = 'pic'>
+                <span>${user}</span>
+            </div>
+        </div>`;
+    }
+});
+
+socket.on('getAllRooms', function (rooms, newRoom) {
+    roomList.innerHTML = '';
+    console.log(rooms);
+    for (var index in rooms) {
+        roomList.innerHTML += `<div class="room_card" id="${rooms[index].name}"
+        onclick="changeRoom('${rooms[index].name}')">
+        <div class="room_item_content">
+            <div class="pic"></div>
+            <div class="roomInfo">
+            <span class="room_name">#${rooms[index].name}</span>
+            <span class="room_author">${rooms[index].creator}</span>
+            </div>
+        </div>
+    </div>`;
+    }
+    document.getElementById(currentRoom).classList.add('active_item');
+});
+
+function changeRoom(room) {
+    if (room !== currentRoom) {
+        socket.emit('updateRooms', room);
+        document.getElementById(currentRoom).classList.remove('active_item');
+        currentRoom = room;
+        document.getElementById(currentRoom).classList.add('active_item');
+    }
+}
--- server.js
+++ server.js
@@ -17,18 +17,52 @@ const rooms = [
 ];
 
 io.on('connection', function (socket) {
+    console.log('New User connected...');
     socket.on('createUser', function (username) {
         socket.username = username;
         usernames[username] = username;
         socket.currentRoom = 'globalChat';
-
         socket.join('globalChat');
         socket.emit('updateChat', 'INFO', 'You have joined global chat');
+        io.sockets.emit('updateUsers', usernames);
+        socket.emit('getAllRooms', rooms, 'globalChat');
     });
     socket.on('sendMessage', function (data) {
         io.sockets
             .to(socket.currentRoom)
             .emit('updateChat', socket.username, data);
+    });
+    socket.on('updateRooms', function (room) {
+        socket.broadcast
+            .to(socket.currentRoom)
+            .emit('updateChat', 'INFO', socket.username + ' left the room ');
+        socket.leave(socket.currentRoom);
+        socket.currentRoom = room;
+        socket.join(room);
+        socket.emit('updateChat', 'INFO', 'You have joined ' + room);
+        socket.broadcast
+            .to(socket.currentRoom)
+            .emit(
+                'updateChat',
+                'INFO',
+                socket.username + ' has joined the room '
+            );
+    });
+    socket.on('createRoom', (newRoom) => {
+        if (newRoom !== null) {
+            rooms.push({
+                name: newRoom,
+                creator: socket.username,
+            });
+            io.sockets.emit('getAllRooms', rooms, null);
+        }
+    });
+    socket.on('disconnect', function () {
+        console.log(`User ${socket.username} disconnected from the server`);
+        delete usernames[socket.username];
+        io.sockets
+            .to(socket.currentRoom)
+            .emit('updateChat', 'INFO', socket.username + ' left the room');
     });
 });
 
